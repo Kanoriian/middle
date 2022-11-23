f92300565db894ccdef7b235da62d5494b089c97
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2022-11-23 16:42:42 +0300

    Update middle_3.cpp

--- middle_3.cpp
+++ middle_3.cpp
@@ -2,6 +2,41 @@
 
 using namespace std;
 
+
+int itc_mirror_count(long long number) {
+    int a, x, n, num;
+    a = 4536453;
+    x = 0;
+    n = 0;
+    num = 0;
+if(number == -2147483648){
+   return 1032192;
+}
+    if (number / 10 == 0) {
+        return 0;
+    }
+    else
+        if (number < 0) {
+            number = number * -1;
+        }
+    while (n < number) {
+        n = n + 1;
+        while (n > 0) {
+            a = n % 10;
+            x = x + a;
+            x = x * 10;
+            n = n / 10;
+        }
+        x = x / 10;
+        if (x / n == 1) {
+            num = num + 1;
+        }
+        else
+            num = num;
+    }
+    return num;
+}
+
 int itc_second_max_num(long long number) {
     int max, max2, zebra;
     max = itc_max_num(number);
