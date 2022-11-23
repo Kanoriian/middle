05909f1749e74ee2abbccc24b03bd7d57812cb61
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2022-11-23 16:42:27 +0300

    Update middle_2.cpp

--- middle_2.cpp
+++ middle_2.cpp
@@ -65,44 +65,25 @@ if (number < 0)
     return x;
 }
 
+long perevorot(int number){
+   int zebra, rezak, n;
+    zebra = 0;
+    n = number;
+if (number < 0)
+number = number * -1;
+ while(number > 0){
+        zebra = number % 10;
+        rezak = rezak * 10 + zebra;
+        number = number / 10;
+ }
+ return rezak;
+}
+
 bool itc_mirror_num(long long number) {
-    int x = itc_rev_num(number);
-    if (x / number == 1) {
+    int zebra, rezak, n;
+    n = perevorot(number);
+    if (number == n)
         return true;
-    }else
         return false;
 }
 
-int itc_mirror_count(long long number) {
-    int a, x, n, num;
-    a = 4536453;
-    x = 0;
-    n = 0;
-    num = 0;
-if(number == -2147483648){
-   return 1032192;
-}
-    if (number / 10 == 0) {
-        return 0;
-    }
-    else
-        if (number < 0) {
-            number = number * -1;
-        }
-    while (n < number) {
-        n = n + 1;
-        while (n > 0) {
-            a = n % 10;
-            x = x + a;
-            x = x * 10;
-            n = n / 10;
-        }
-        x = x / 10;
-        if (x / n == 1) {
-            num = num + 1;
-        }
-        else
-            num = num;
-    }
-    return num;
-}
